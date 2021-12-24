82ff2e7331919b031ae74313371b315baa71c04b
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-12-24 18:31:05 +0100

    Statistics Types

---
+++ src/main/java/nl/codevs/bwstat/data/BWStatType.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the bwstat distribution (https://github.com/CocoTheOwner/bwstat).
+ * Copyright (c) 2021 Sjoerd van de Goor.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package nl.codevs.bwstat.data;
+
+/**
+ * Stored statistic types
+ *
+ * @author Sjoerd van de Goor
+ * @since 24/12/2021
+ */
+public enum BWStatType {
+     finalKills,
+     finalDeaths,
+     level,
+     games,
+     wins,
+     losses,
+     streak,
+     kills,
+     deaths
+}
